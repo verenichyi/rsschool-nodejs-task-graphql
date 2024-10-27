4af4072036e5fa2b974bcdcac873f6f7d78bebaf
Yury_Viarenich <verenichyi@gmail.com>
2024-10-27 15:40:52 +0300

    feat: add empty query/mutations, add depthLimit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  graphql,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  parse,
+  validate,
+} from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -14,8 +22,39 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      // return graphql();
+    handler: async function (request) {
+      const query = new GraphQLObjectType({
+        name: 'Mutation',
+        fields: () => {
+          return {};
+        },
+      });
+
+      const mutation = new GraphQLObjectType({
+        name: 'Query',
+        fields: () => {
+          return {};
+        },
+      });
+
+      const schema = new GraphQLSchema({ query, mutation });
+
+      const depthErrors = validate(schema, parse(String(request.body.query)), [
+        depthLimit(5),
+      ]);
+
+      if (depthErrors.length) {
+        return {
+          errors: depthErrors,
+        };
+      }
+
+      return await graphql({
+        schema,
+        source: String(request.body.query),
+        contextValue: fastify,
+        variableValues: request.body.variables,
+      });
     },
   });
 };
